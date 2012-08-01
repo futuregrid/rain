388156126bb18528558fbb46ca554f5d328c7bb7
Javier Diaz <javier.diazmontes@gmail.com>
2012-08-01 10:15:25 -0400

    rain

--- src/futuregrid/image/repository/client/IRServiceProxy.py
+++ src/futuregrid/image/repository/client/IRServiceProxy.py
@@ -211,7 +211,7 @@ class IRServiceProxy(object):
         self._connIrServer.write(msg)
         if self.check_auth(userId, checkauthstat):
             #wait for output
-            output = self._connIrServer.read(64000)            
+            output = self._connIrServer.read(327680)            
             if output == "None":
                 output = None
         else:
